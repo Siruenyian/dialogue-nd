a9639af3fe064b4d0bd97a7a2ae3eab1529e0141
Siruenyian <92420947+Siruenyian@users.noreply.github.com>
2024-01-05 22:19:54 +0700

    base system commit

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "NPC")]
+public class NPC : ScriptableObject
+{
+    public DialogueObj npcDialogue;
+    public Sprite artworkleft;
+    public Sprite artworkright;
+    public Sprite bgart;
+}
